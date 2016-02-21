add8972b7171ac41d78327e6b883e11c4c9adfd8
danielps1 <daniel.pasch.s@gmail.com>
2016-02-21 22:00:55 +0100

    clean code a bit and remove unneeded stuff

--- main.go
+++ main.go
@@ -1,68 +1,53 @@
 package main
 
-import ("net"
-        "log"
-        "bufio"
-        "fmt"
-        "net/textproto"
-      )
-
-var channels = [...] string {"#pajlada","#nymn_hs"}
-
-
-type Bot struct{
-    server string
-    port string
-    nick string
-    pread, pwrite chan string
-    conn net.Conn
-}
-
-func NewBot() *Bot {
-    return &Bot{
-        server: "irc.twitch.tv",
-        port: "6667",
-        nick: "justinfan321314364545123142435",
-        conn: nil,
-    }
-}
-
-func (bot *Bot) Connect() (conn net.Conn, err error) {
-    conn, err = net.Dial("tcp", bot.server + ":" + bot.port)
-    if err != nil {
-        log.Fatal("[ERROR] ", err)
-    }
-    bot.conn = conn
-    return bot.conn, nil
-}
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"net/textproto"
+)
 
 func joinChannel(conn net.Conn, channel string) {
-    fmt.Fprintf(conn, "JOIN %s\r\n", channel)
+	fmt.Fprintf(conn, "JOIN %s\r\n", channel)
+}
+func main() {
+	handleConn()
 }
 
-func main(){
-    ircbot := NewBot()
-    conn, _ := ircbot.Connect()
-    fmt.Fprintf(conn, "USER %s 8 * :%s\r\n", ircbot.nick, ircbot.nick)
-    fmt.Fprintf(conn, "NICK %s\r\n", ircbot.nick)
+func sendMessage(conn net.Conn, channel string, message string) {
+	fmt.Fprintf(conn, "PRIVMSG %s :%s\r\n", channel, message)
+}
 
+func handleConn() {
+	var conn net.Conn
+	server := "irc.twitch.tv:6667"
+	nick := "gempir"
+	pass := ""
+	channel := "#nymn_hs"
 
-    for index,element := range channels {
-        fmt.Print(index)
-        joinChannel(conn, element)
-    }
+	conn, err := net.Dial("tcp", server)
+	if err != nil {
+		log.Fatal("unable to connect to IRC server ", err)
+	}
 
-    defer conn.Close()
+	fmt.Fprintf(conn, "PASS %s\r\n", pass)
+	fmt.Fprintf(conn, "USER %s\r\n", nick)
+	fmt.Fprintf(conn, "NICK %s\r\n", nick)
 
-    reader := bufio.NewReader(conn)
-    tp := textproto.NewReader( reader )
+	joinChannel(conn, channel)
 
-    for {
-        line, err := tp.ReadLine()
-        if err != nil {
-            break // break loop on errors
-        }
-        fmt.Printf("%s\n", line)
-    }
+	sendMessage(conn, channel, "Kappa")
 
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	tp := textproto.NewReader(reader)
+
+	for {
+		line, err := tp.ReadLine()
+		if err != nil {
+			break // break loop on errors
+		}
+		fmt.Printf("%s\n", line)
+	}
 }
