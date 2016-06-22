919aa058cfc4f064f63e9a4345e55fb183410c41
nuuls <nilsv21@gmail.com>
2016-06-22 22:18:40 +0200

    renamed username to nick

--- client.go
+++ client.go
@@ -8,7 +8,7 @@ import (
 type Client struct {
 	incomingConn net.Conn
 	pass         string
-	username     string
+	nick         string
 }
 
 func newClient(conn net.Conn) Client {
@@ -22,7 +22,9 @@ func (c *Client) handleMessage(line string) {
 	spl := strings.SplitN(line, " ", 2)
 	// irc command
 	switch spl[0] {
-		case "PASS":
-			c.pass = spl[1]
+	case "PASS":
+		c.pass = spl[1]
+	case "NICK":
+		c.nick = strings.ToLower(spl[1]) // make sure the nick is lowercase
 	}
 }
--- client_test.go
+++ client_test.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestCanHandlePass(t *testing.T) {
-    c := new(Client)
-    c.handleMessage("PASS test;oauth:123test")
-    if c.pass != "test;oauth:123test" {
-        log.Fatal("pass doesn't match")
-    } 
+	c := new(Client)
+	c.handleMessage("PASS test;oauth:123test")
+	if c.pass != "test;oauth:123test" {
+		log.Fatal("pass doesn't match")
+	}
+	c.handleMessage("NICK Nuuls")
+	if c.nick != "nuuls" {
+		log.Fatal("nick doesn't match")
+	}
 }
