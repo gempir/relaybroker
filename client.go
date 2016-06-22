dc20d1ab8ef028a98efbabd07edb262c6033ad0e
nuuls <nilsv21@gmail.com>
2016-06-22 21:48:56 +0200

    useless variable

--- client.go
+++ client.go
@@ -19,9 +19,9 @@ func newClient(conn net.Conn) Client {
 
 func (c *Client) handleMessage(line string) {
 	log.Debug(line)
-	spl := strings.Split(line, " ")
-	command := spl[0]
-	switch command {
+	// spl[0] is the command and spl[1] the rest of the message
+	spl := strings.SplitN(line, " ", 2)
+	switch spl[0] {
 	case "PASS":
 		c.pass = spl[1]
 	}
