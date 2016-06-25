06451c9d92bc44ea7fdb523f20ec2a5ed37e8f67
gempir <daniel.pasch.s@gmail.com>
2016-06-25 10:55:16 +0200

    test for new connection

--- twitch.go
+++ twitch.go
@@ -4,10 +4,23 @@ import "net"
 
 // Connection simple type for connection to twitch
 type Connection struct {
-	msgCount int // number of messages in 30 seconds
-	conn     net.Conn
+	msgCount      int // number of messages in 30 seconds
+	conn          net.Conn
+	channels      []string
+	authenticated bool
+	alive         bool
 }
 
-func newConnection(nick, pass string) {
+func newConnection(nick string, pass string) Connection {
+	return Connection{
+		msgCount:      0,
+		conn:          nil,
+		channels:      nil,
+		authenticated: false,
+		alive:         false,
+	}
+}
+
+func connect(ct Connection) {
 
 }
--- twitch_test.go
+++ twitch_test.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCanCreateNewConnection(t *testing.T) {
+	ct := newConnection("gempir", "password")
+	if reflect.TypeOf(ct).String() != "main.Connection" {
+		t.Fatal("new connection of wrong type", reflect.TypeOf(ct).String())
+	}
+}
