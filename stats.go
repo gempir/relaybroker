b97bb90480e63b05735d788c34fce960588d241d
nuuls <nilsv21@gmail.com>
2016-07-08 15:26:17 +0200

    better uptime formatting for the API

--- stats.go
+++ stats.go
@@ -21,24 +21,13 @@ func statsAPI() {
 }
 
 func apiData(w http.ResponseWriter, r *http.Request) {
-	ago := time.Since(stats.startTime)
-	mins := int(ago.Minutes())
 
-	m := mins % 60
-	hours := (mins - m) / 60
-
-	h := hours % 24
-	days := (hours - h) / 24
-	hours = hours - days*24
-
-	mins = m
-	uptime := fmt.Sprintf("%dd %dh %dm", days, hours, mins)
 	botCount := fmt.Sprintf("%d bot", len(bots))
 	if len(bots) > 1 {
 		botCount += "s"
 	}
 	s := fmt.Sprintf("relaybroker stats: online for %s, %s connected, %d total connections, %d messages sent, %d messages received MrDestructoid ",
-		uptime,
+		getUptime(),
 		botCount,
 		countConns(),
 		stats.totalMsgsSent,
@@ -52,4 +41,30 @@ func countConns() int {
 		i += len(bot.sendconns) + len(bot.readconns) + 1
 	}
 	return i
+}
+
+func getUptime() string {
+	ago := time.Since(stats.startTime)
+	totalSeconds := int(ago.Seconds())
+	s := totalSeconds % 60
+	mins := (totalSeconds - s) / 60
+	seconds := totalSeconds - mins*60
+	m := mins % 60
+	mins = m
+	hours := (mins - m) / 60
+
+	h := hours % 24
+	days := (hours - h) / 24
+	hours = hours - days*24
+
+	if mins+hours+days < 1 {
+		// up for less than 1 minute
+		return fmt.Sprintf("%ds", seconds)
+	} else if hours+days < 1 {
+		// up for less than 1 hour
+		return fmt.Sprintf("%dm %ds", mins, seconds)
+	} else if days < 1 {
+		return fmt.Sprintf("%dh %dm %ds", hours, mins, seconds)
+	}
+	return fmt.Sprintf("%dd %dh %dm", days, hours, mins)
 }
